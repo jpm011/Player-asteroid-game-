dae5568ff67687428f860b44cfcb0fc5a4181f08
jay <jay@thebatcave.com>
2020-01-23 11:25:21 -0600

    Initial Commit

---
+++ Assignment1_115/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1_115
+//
+//  Created by jay patel on 2020-01-23.
+//  Copyright © 2020 jay patel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
